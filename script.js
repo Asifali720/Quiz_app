9826b6317ea69b2561edb7d18691b44b70a380fc
Asifali720 <asiflashari720@gmail.com>
2024-04-01 01:03:30 +0500

    new

--- script.js
+++ script.js
@@ -1,41 +1,4 @@
-const quizData = [
-    {
-        question: 'what is the best language for front end in web development?',
-        a: 'Python',
-        b: 'Ruby',
-        c: 'Java script',
-        d: 'Java',
-        correct: 'c'
-    }, {
-        question: 'what is stand for HTML?',
-        a: 'hello typer mail limit',
-        b: 'harms text markup language',
-        c: 'developer language',
-        d: 'hyper text markup language',
-        correct: 'd'
-    }, {
-        question: 'CSS use in?',
-        a: 'backend',
-        b: 'frontend',
-        c: 'none of them',
-        d: 'may be in server',
-        correct: 'b'
-    }, {
-        question: 'Are java and java script is same?',
-        a: 'never',
-        b: 'yes its but only diffrence name',
-        c: 'exactly same',
-        d: 'its not a language',
-        correct: 'a'
-    }, {
-        question: 'can you change div class with your own thinks or not?',
-        a: 'no because div have specific class',
-        b: 'may be',
-        c: 'yes you can change class with your own words like div.container',
-        d: 'yes you change it with name by section.container',
-        correct: 'c'
-    }
-];
+
 
 const answerEls = document.querySelectorAll('.answer')
 const quiz = document.getElementById('quiz')
@@ -46,50 +9,102 @@ const b_text = document.getElementById('b_text');
 const c_text = document.getElementById('c_text');
 const d_text = document.getElementById('d_text');
 
-let currentQuiz = 0;
-let score = 0;
+ const data = [
+    {
+       question: "who was founder of Pakistan",
+       a: "Allama Iqbal",
+       b: "Quaid e Azam",
+       c: "Chaudry Rehmat Ali",
+       d: "Tipu Sultan",
+       correct: "b"
+    },
+    {
+        question: "who seen dream for Pakistan",
+        a: "Tipu Sultan",
+        b: "Quaid e Azam",
+        c: "Allama Iqbal",
+        d: "Chaudry Rahmat Ali",
+        correct: "c"
+     },
+     {
+        question: "which month and year of pass revolution of Pakistan",
+        a: "17 march, 1999",
+        b: "29 march, 1920",
+        c: "23 march, 1990",
+        d: "23 march, 1940",
+        correct: "d"
+     },
+     {
+        question: "which year Pakistan become an independent state",
+        a: "1947",
+        b: "1948",
+        c: "1940",
+        d: "1945",
+        correct: "a"
+     }, {
+        question: "what is pakistanis national language",
+        a: "punjabi",
+        b: "hindi",
+        c: "urdu",
+        d: "persion",
+        correct: "c"
+     },
+ ]
 
-loadQuiz();
 
-function loadQuiz() {
-    const currentQuizData = quizData[currentQuiz];
+ let currentQuiz = 0
+ let score = 0
 
-    questionEl.innerText = currentQuizData.question;
-    a_text.innerHTML = currentQuizData.a;
-    b_text.innerHTML = currentQuizData.b;
-    c_text.innerHTML = currentQuizData.c;
-    d_text.innerHTML = currentQuizData.d;
+ loadQuiz()
+ function loadQuiz() {
+   const currentQuizData = data[currentQuiz]
+   deSelect()
 
-    // currentQuiz++;
+   questionEl.innerText = currentQuizData.question
+   
+   a_text.innerText = currentQuizData.a
+   b_text.innerText = currentQuizData.b
+   c_text.innerText = currentQuizData.c
+   d_text.innerText = currentQuizData.d
 
-}
+ }
 
-function getSelected() {
-    let answer = undefined;
 
-    answerEls.forEach((answerEl) => {
-        if (answerEl.checked) {
-            answer = answerEl.id;
-        }
-    });
-    return answer;
-}
 
-function handleSubmit() {
-    const answer = getSelected();
-    if (answer) {
-        if (answer === quizData[currentQuiz].correct) {
-            score++;
-        }
-    }
-    currentQuiz++;
-    // loadQuiz();
-    if (currentQuiz < quizData.length) {
-        loadQuiz();
-    } else {
-        quiz.innerHTML = `<h2>You answer correctly at ${score}/${quizData.length} questions.</h2><button onClick="location.reload()">Reload</button>`
-    }
-}
+ function getSelected(){
+   let answer = undefined;
+   answerEls.forEach((answerEl)=>{
+      if(answerEl.checked){
+         answer = answerEl.id
+      }
+   })
+   return answer
+ }
+ function deSelect(){
+   answerEls.forEach((answerEl)=>{
+      answerEl.checked = false
+   })
+ }
+
+ function refreshPage(){
+   window.location.reload()
+ }
 
 const submitBtn = document.getElementById('submit');
-submitBtn.addEventListener('click', handleSubmit);
+submitBtn.addEventListener('click', ()=>{
+
+   const answerQuiz = getSelected()
+   if(answerQuiz){
+      if(answerQuiz == data[currentQuiz].correct){
+           score ++;
+      }
+      currentQuiz++
+      if(currentQuiz < data.length){
+         loadQuiz()
+      } else{
+         quiz.innerHTML = `<h2>Your Correct answer is ${score}/${data.length}</h2>
+         <button onClick="refreshPage()">Reload</button>`
+      }
+   }
+  
+});
